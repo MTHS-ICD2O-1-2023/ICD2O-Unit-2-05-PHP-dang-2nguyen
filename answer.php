4604ef15e82da1ddf7adfa3178b9c6e8ee2640ee
Hoang Thien Dang Nguyen <159185239+dang-2nguyen@users.noreply.github.com>
2024-03-26 17:52:51 +0000

    aefrweafrws

--- answer.php
+++ answer.php
@@ -16,6 +16,7 @@
   <link rel="manifest" href="site.webmanifest" />
   <title>Salary Program,in PHP</title>
 </head>
+
 <body>
   <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
@@ -25,29 +26,27 @@
       </div>
     </header>
     <main class="mdl-layout__content">
-      <br />
-      <div class="page-content">Enter your employment information.</div>
       <div class="page-content-php">
-        <form action="answer.php" method="POST">
-          <div class="mdl-textfield mdl-js-textfield">
-            <input class="mdl-textfield__input" type=" text" pattern="-?[0-9]*(\.[0-9]+)?" name="the-time">
-            <label class="mdl-textfield__label" for="the-time">hour you works (h)</label>
-            <span class="mdl-textfield__error">Input is not a number!</span>
-          </div>
-          <br />
-          <div class="mdl-textfield mdl-js-textfield">
-            <input class="mdl-textfield__input" type=" text" pattern="-?[0-9]*(\.[0-9]+)?" name="salary">
-            <label class="mdl-textfield__label" for="salary">salary for every hour ($)</label>
-            <span class="mdl-textfield__error">Input is not a number!</span>
-          </div>
-          <br />
-          <!-- Accent-colored raised button with ripple -->
-          <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" type="submit">
-            Calculate
-          </button>
-        </form>
+        <div id="user-info">
+          <?php
+          $time = $_POST["the-time"];
+          $money = $_POST["salary"];
+
+          // process
+          $pay = ($time * $money) * (1.00 - 0.18);
+          $government = ($time * $money) * 0.18;
+
+          // output
+          echo "If you work in " . $time . " and you get " . $money . " in every hour.";
+          echo "<br />";
+          echo "<br />";
+          echo "Your pay will be " . '$ ' . sprintf('%0.2f', $pay);
+          echo "the government will take " . '$ ' . sprintf('%0.2f', $government);
+          ?>
+        </div>
       </div>
     </main>
   </div>
 </body>
+
 </html>
